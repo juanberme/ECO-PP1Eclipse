46db10fc9396c3f700168cf0cc88a12d9465e9f2
Laura <isabelaleon8@gmail.com>
2020-10-23 15:16:58 -0500

    imagenes

--- PP1Servidor/src/view/Main.java
+++ PP1Servidor/src/view/Main.java
@@ -11,10 +11,25 @@ import model.Asteroides;
 import model.Bicho;
 import model.Pulpo;
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Main extends PApplet implements onMessageListener{
+	int pantalla;
 
-
+	private PImage menu1;
+	private PImage menu2;
+	private PImage pantalla1;
+	private PImage ganador;
+	private PImage instrucciones;
+	private PImage aste;
+	private PImage pulpito;
+	private PImage bichin;
+	private PImage naveAzul;
+	private PImage naveVerde;
+	private PImage naveVioleta;
+	private PImage naveRosa;
+	private PImage vidaAzul;
+	private PImage vidaVioleta;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -35,6 +50,25 @@ public class Main extends PApplet implements onMessageListener{
 	}
 	
 	public void setup() {
+		pantalla = 3;
+		
+		//imagenes
+		menu1 = loadImage ("images/menu1.png");
+		menu2= loadImage ("images/menu2.png");
+		pantalla1= loadImage ("images/pantalla1.png");
+		ganador= loadImage ("images/ganador.png");
+		instrucciones = loadImage ("images/instrucciones.png");
+		aste= loadImage ("images/asteroide.png");
+		pulpito= loadImage ("images/pulpo.png");
+		bichin= loadImage ("images/ojo.png");
+		naveAzul = loadImage ("images/naveAzul.png");
+		naveVerde= loadImage ("images/naveVerde.png");
+		naveVioleta= loadImage ("images/naveVioleta.png");
+		naveRosa= loadImage ("images/naveRosa.png");
+		vidaAzul= loadImage ("images/vidaAzul.png");
+		vidaVioleta= loadImage ("images/VidaVioleta.png");
+		
+	// tcp
 		tcp = TCPSingleton.getInstance();
 		tcp.setObserver(this);
 		tcp.start();
@@ -78,47 +112,73 @@ public class Main extends PApplet implements onMessageListener{
 		bic.add(new  Bicho(800,600));
 		bic.add(new  Bicho(1000,450));	
 		
+		
+		
 	}
 	
 	public void draw() {
 		background(0);
+		switch (pantalla) {
+			case 1:
+				//para ir a juego
+				image (menu1,0,0);
+				
+				break;
+			case 2:
+				//para ir a instruciones
+				image (menu2,0,0);
+				
+				break;
+			case 3:
+				//pantalla de juego
+				image (pantalla1,0,0);
 
-		
-		
-		
-		for (int i = 0; i < ast.size(); i++) {
-			Asteroides as = ast.get(i);
-			as.mover();
-			fill(255);
-			ellipse(as.getAx(), as.getAy(), 50, 50);
-		}
-		
-		for (int o = 0; o < pulp.size(); o++) {
-			Pulpo pu = pulp.get(o);
-			pu.mover();
-			fill(255,200,200);
-			ellipse(pu.getPx(), pu.getPy(), 60, 60);
-		}
-		
-		for (int n = 0; n < bic.size(); n++) {
-			Bicho bi = bic.get(n);
-			bi.mover();
-			fill(0,200,200);
-			ellipse(bi.getBx()+20, bi.getBy()+20, 70, 70);
-		}
-		
-		
+				for (int i = 0; i < ast.size(); i++) {
+					Asteroides as = ast.get(i);
+					as.mover();
+					
+					image(aste, as.getAx(), as.getAy(), 150,150);
+				}
+				
+				for (int o = 0; o < pulp.size(); o++) {
+					Pulpo pu = pulp.get(o);
+					pu.mover();
+					
+					image(pulpito,pu.getPx(), pu.getPy(), 80, 80);
+				}
+			
+				for (int n = 0; n < bic.size(); n++) {
+					Bicho bi = bic.get(n);
+					bi.mover();
+					fill(0,200,200);
+					image(bichin,bi.getBx()+20, bi.getBy()+20, 80, 80);
+				}
+				
+				
 
-		/*for(int i = 0; i < 2; i++ ) {
-			Session sesion = tcp.getSesiones().get(i);
-		}*/
-		ArrayList<Session> sesiones = tcp.getSesiones();
-		for(int i = 0; i < sesiones.size(); i++) {
-			Coordenada c = sesiones.get(i).getCoordenada();
-			fill(0,0,255);
-			ellipse(c.getX(), c.getY() , 100, 100);
+				/*for(int i = 0; i < 2; i++ ) {
+					Session sesion = tcp.getSesiones().get(i);
+				}*/
+				ArrayList<Session> sesiones = tcp.getSesiones();
+				for(int i = 0; i < sesiones.size(); i++) {
+					Coordenada c = sesiones.get(i).getCoordenada();
+					
+					image(naveAzul,c.getX(), c.getY() , 100, 100);
+				}
+				
+				break;
+			case 4: 
+				//instrucciones
+				image(instrucciones,0,0);
+				
+			break; 
+			case 5: 
+				//ganador
+				image (ganador,0,0);
+				break;
+			
 		}
-		
+
 
 	}
 
@@ -131,6 +191,9 @@ public class Main extends PApplet implements onMessageListener{
 		y = coorRecibida.getY();*/
 	}
 
+public void mousePressed() {
+	
+}
 
 
 }
